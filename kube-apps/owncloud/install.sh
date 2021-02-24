890208970cf45c909be0fe7b572242fbbcb7b2a7
Paul Mullins <paulmullins@protonmail.com>
2021-02-24 12:16:58 -0500

    Updated files for owncloud app

--- kube-apps/owncloud/install.sh
+++ kube-apps/owncloud/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-helm repo add gitea-charts https://dl.gitea.io/charts/
+helm install -f owncloud-values.yaml owncloud bitnami/owncloud
+
 helm repo update
 helm install -f gitea-values.yaml gitea gitea-charts/gitea
 kubectl apply -f gitea-ingress.yaml
