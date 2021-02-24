386facdee3a9770f360e9402d2a50f501921c006
Paul Mullins <paulmullins@protonmail.com>
2021-02-24 15:04:34 -0500

    Repeated getting nextcloud helm chart working

--- kube-apps/nextcloud/install-nextcloud.sh
+++ kube-apps/nextcloud/install-nextcloud.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 helm repo add nextcloud https://nextcloud.github.io/helm/
 helm repo update
-# helm show values nextcloud/nextcloud > nextcloud-values.yaml
-kubectl apply -f nextcloud-pv.yaml
-kubectl apply -f nextcloud-pvc.yaml
 helm install -f nextcloud-values.yaml nextcloud nextcloud/nextcloud
 kubectl apply -f nextcloud-ingress.yaml
 
+# helm show values extcloud/nextcloud > test.yaml
 
-helm install --name my-release \
-  --set nextcloud.username=admin,nextcloud.password=password,mariadb.rootUser.password=secretpassword \
-    stable/nextcloud
+
+#helm install --name nextcloud \
+#  --set nextcloud.username=admin,nextcloud.password=password,mariadb.rootUser.password=secretpassword \
+#    stable/nextcloud
