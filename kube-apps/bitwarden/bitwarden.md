88b19cf2d430aeada3cde7c6f4b092cadca87847
Paul Mullins <paulmullins@protonmail.com>
2021-03-16 18:22:23 -0400

    Added bitwarden app

---
+++ kube-apps/bitwarden/bitwarden.md
@@ -0,0 +1,3 @@
+helm install k8s-at-home/bitwardenrs
+
+helm show values k8s-at-home/bitwardenrs > bitwarden-values.yaml
---
+++ kube-apps/bitwarden/install-bitwarden.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+helm repo add k8s-at-home https://k8s-at-home.com/charts/
+helm install k8s-at-home/bitwardenrs
