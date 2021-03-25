b3e9b6f92f0894bd40bd02aa8aa4567e6b3190e8
Paul Mullins <paulmullins@protonmail.com>
2021-03-25 19:06:53 -0400

    about to test external-dns

--- kube-apps/external-dns/install-extenral-dns.sh
+++ kube-apps/external-dns/install-extenral-dns.sh
@@ -2,4 +2,5 @@
 export $(egrep -v '^#' ../../.env | xargs)
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
-envsubst < trilium-notes-values.yaml > prod.trilium-notes-values.yaml
+envsubst < cloud-flare.yaml > prod.cloud-flare.yaml
+# helm install -f prod.cloud-flare.yaml external-dns bitnami/external-dns
