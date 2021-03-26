a63fe64261dc30d20821695a7cb13c6097dcb3e4
Paul Mullins <paulmullins@protonmail.com>
2021-03-25 20:52:32 -0400

    mend

--- kube-apps/nextcloud/install-nextcloud.sh
+++ kube-apps/nextcloud/install-nextcloud.sh
@@ -3,7 +3,7 @@ export $(egrep -v '^#' ../../.env | xargs)
 helm repo add nextcloud https://nextcloud.github.io/helm/
 helm repo update
 envsubst < nextcloud-values.yaml > prod.nextcloud-values.yaml
-helm install -f nextcloud-values.yaml nextcloud nextcloud/nextcloud
+helm install -f prod.nextcloud-values.yaml nextcloud nextcloud/nextcloud
 # kubectl apply -f nextcloud-ingress.yaml
 
 # helm show values extcloud/nextcloud > test.yaml
